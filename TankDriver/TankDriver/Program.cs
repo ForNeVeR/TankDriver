236d9e41e939c925a54f45dab739622bb49a0e76
ForNeVeR <neverthness@gmail.com>
2012-01-06 18:59:44 +0700

    Converted spaces in code to tabs.

--- TankDriver/TankDriver/Program.cs
+++ TankDriver/TankDriver/Program.cs
@@ -1,21 +1,18 @@
-using System;
-
 namespace TankDriver
 {
 #if WINDOWS || XBOX
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main(string[] args)
-        {
-            using (Game game = new Game())
-            {
-                game.Run();
-            }
-        }
-    }
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+			using (Game game = new Game())
+			{
+				game.Run();
+			}
+		}
+	}
 #endif
 }
-
