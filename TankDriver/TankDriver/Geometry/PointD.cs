220ec56365a53423ce3a514c31ad92da6f998421
ForNeVeR <neverthness@gmail.com>
2012-01-06 21:51:12 +0700

    Added geometry helper classes.

---
+++ TankDriver/TankDriver/Geometry/PointD.cs
@@ -0,0 +1,22 @@
+﻿namespace TankDriver.Geometry
+{
+	/// <summary>
+	/// Point with double coordinates.
+	/// </summary>
+	struct PointD
+	{
+		public readonly double X;
+		public readonly double Y;
+
+		public PointD(double x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public PointD MovedByVector(VectorD vector)
+		{
+			return new PointD(X + vector.X, Y + vector.Y);
+		}
+	}
+}
