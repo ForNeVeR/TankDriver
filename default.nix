f49f1bfbc4e5ea78f1739680df140448870de199
Friedrich von Never <friedrich@fornever.me>
2016-07-31 20:22:16 +0700

    default.nix: proper variant

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 with import <nixpkgs> {}; {
     TankDriverEnv = stdenv.mkDerivation {
         name = "TankDriver";
-        buildInputs = [ mono libX11 ];
+        buildInputs = [ mesa mono dotnetPackages.Nuget xorg.libX11 ];
+        LD_LIBRARY_PATH="${xorg.libX11}/lib:${mesa}/lib";
     };
 }
