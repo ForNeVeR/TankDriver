6dd5c7f0a991d6ec1e0d07ec34a853dd75c68aac
rexim <reximkut@gmail.com>
2016-08-27 04:16:49 +0600

    Add Bullet stubs (#5)
    MonoDevelop also change the csproj file. Not sure if those changes are
    important or not but decided to commit them anyway.

---
+++ TankDriver.App/Logic/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TankDriver.Models;
+
+namespace TankDriver.Logic
+{
+	internal class Bullet: IUnit
+	{
+		private readonly BulletModel _model;
+
+		public Bullet (double x, double y)
+		{
+			_model = new BulletModel (this);
+		}
+
+		/// <summary>
+		/// Unit model.
+		/// </summary>
+		/// <returns>Reference ti this unit's model.</returns>
+		public IModel GetModel()
+		{
+			return _model;
+		}
+	}
+}
+
---
+++ TankDriver.App/Models/BulletModel.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using TankDriver.Logic;
+
+namespace TankDriver.Models
+{
+	/// <summary>
+	/// Graphical model of a bullet.
+	/// </summary>
+	internal class BulletModel: IModel
+	{
+		/// <summary>
+		/// Bullet associated with this bullet.
+		/// </summary>
+		private readonly Bullet _bullet;
+
+		/// <summary>
+		/// Texture for this bullet.
+		/// </summary>
+		private TextureStorage _textureStorage;
+
+		/// <summary>
+		/// Model constructor.
+		/// </summary>
+		/// <param name="bullet">Bullet for this model</param>
+		public BulletModel (Bullet bullet)
+		{
+			_bullet = bullet;
+		}
+
+		/// <summary>
+		/// Returns unit associated with this model.
+		/// </summary>
+		/// <returns>Unit.</returns>
+		public IUnit GetUnit()
+		{
+			return _bullet;
+		}
+
+		public void LoadTextures(TextureStorage textureStorage)
+		{
+			_textureStorage = textureStorage;
+		}
+
+		/// <summary>
+		/// Renders model.
+		/// </summary>
+		/// <param name="spriteBatch">Object for performing drawing actions.</param>
+		public void Render(SpriteBatch spriteBatch)
+		{
+		}
+	}
+}
+
