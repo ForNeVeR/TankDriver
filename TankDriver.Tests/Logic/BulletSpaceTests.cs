cb16277127675772fddfb0e3c880ae1d8dfce691
rexim <reximkut@gmail.com>
2016-09-05 22:49:55 +0600

    Add probe test for bullet space (#5)

---
+++ TankDriver.Tests/Logic/BulletSpaceTests.cs
@@ -0,0 +1,21 @@
+﻿using TankDriver.Logic;
+using Microsoft.Xna.Framework;
+using Xunit;
+
+namespace TankDriver.Tests.Logic
+{
+    public class BulletSpaceTests
+    {
+        [Fact]
+        public void BulletOutOfBound()
+        {
+            BulletSpace bulletSpace = new BulletSpace(new Rectangle(0, 0, 100, 100));
+            bulletSpace.AddBullet(50.0, 50.0, 0.0);
+            bulletSpace.AddBullet(200.0, 200.0, 0.0);
+            Assert.Equal(2, bulletSpace.Bullets.Count);
+            bulletSpace.Update(System.TimeSpan.FromMilliseconds(10.0));
+            Assert.Equal(1, bulletSpace.Bullets.Count);
+        }
+    }
+}
+
