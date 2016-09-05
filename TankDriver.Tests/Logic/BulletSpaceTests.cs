348cf1d3192926f32aecb8567e1e390bb36d28d8
rexim <reximkut@gmail.com>
2016-09-05 22:58:06 +0600

    Add test for shooting into space (#5)

--- TankDriver.Tests/Logic/BulletSpaceTests.cs
+++ TankDriver.Tests/Logic/BulletSpaceTests.cs
@@ -6,15 +6,27 @@ namespace TankDriver.Tests.Logic
 {
     public class BulletSpaceTests
     {
+        private readonly Rectangle BulletSpaceBounds = new Rectangle(0, 0, 100, 100);
+
         [Fact]
         public void BulletOutOfBound()
         {
-            BulletSpace bulletSpace = new BulletSpace(new Rectangle(0, 0, 100, 100));
+            BulletSpace bulletSpace = new BulletSpace(BulletSpaceBounds);
             bulletSpace.AddBullet(50.0, 50.0, 0.0);
             bulletSpace.AddBullet(200.0, 200.0, 0.0);
             Assert.Equal(2, bulletSpace.Bullets.Count);
             bulletSpace.Update(System.TimeSpan.FromMilliseconds(10.0));
             Assert.Equal(1, bulletSpace.Bullets.Count);
+        }
+
+        [Fact]
+        public void ShootBulletIntoSpace()
+        {
+            BulletSpace bulletSpace = new BulletSpace(BulletSpaceBounds);
+            Tank tank = new Tank(20.0, 20.0, 0.0);
+            tank.ShootInto(bulletSpace);
+            Assert.Equal(1, bulletSpace.Bullets.Count);
+            Assert.Equal(tank.TurretHeading, bulletSpace.Bullets[0].Heading, Configuration.GeometryPrecision);
         }
     }
 }
