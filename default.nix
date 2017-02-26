211e7b32661d4122563195ce280c8cadb1e07459
rexim <reximkut@gmail.com>
2017-02-26 22:35:09 +0700

    Use mono 4.6 available since 16.09

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {}; {
     TankDriverEnv = stdenv.mkDerivation {
         name = "TankDriver";
-        buildInputs = [ mesa mono dotnetPackages.Nuget xorg.libX11 ];
+        buildInputs = [ mesa mono46 dotnetPackages.Nuget xorg.libX11 ];
         LD_LIBRARY_PATH="${xorg.libX11}/lib:${mesa}/lib";
     };
 }
